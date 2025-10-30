b7e77f5f961079681c0e5616f9af9e3d3509c0f5
kovacsgergely22 <113249160+kovacsgergely22@users.noreply.github.com>
2025-10-30 12:51:31 +0100

    add testcase

--- src/test/java/uni/miskolc/RoverTest.java
+++ src/test/java/uni/miskolc/RoverTest.java
@@ -15,4 +15,43 @@ public class RoverTest {
         // Az ellenőrzés is az enumra történik
         assertEquals(Direction.NORTH, rover.getDirection());
     }
+
+    @Test
+    void testRoverTurnsRightFromNorth() {
+        Rover rover = new Rover(0, 0, Direction.NORTH);
+        rover.receiveCommand('r');
+        assertEquals(0, rover.getX());
+        assertEquals(0, rover.getY());
+        assertEquals(Direction.NORTH, rover.getDirection());
+    }
+
+    /**
+     * Teszt: Keletről (E) jobbra fordulva Délre (S) kell nézzen.
+     */
+    @Test
+    void testRoverTurnsRightFromEast() {
+        Rover rover = new Rover(0, 0, Direction.EAST);
+        rover.receiveCommand('r');
+        assertEquals(Direction.SOUTH, rover.getDirection());
+    }
+
+    /**
+     * Teszt: Délről (S) jobbra fordulva Nyugatra (W) kell nézzen.
+     */
+    @Test
+    void testRoverTurnsRightFromSouth() {
+        Rover rover = new Rover(0, 0, Direction.SOUTH);
+        rover.receiveCommand('r');
+        assertEquals(Direction.WEST, rover.getDirection());
+    }
+
+    /**
+     * Teszt: Nyugatról (W) jobbra fordulva Északra (N) kell nézzen.
+     */
+    @Test
+    void testRoverTurnsRightFromWest() {
+        Rover rover = new Rover(0, 0, Direction.WEST);
+        rover.receiveCommand('r');
+        assertEquals(Direction.NORTH, rover.getDirection());
+    }
 }

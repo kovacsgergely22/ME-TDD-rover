0db0558a5d42a61c443e6f8cfc860ad47c5c2f5c
kovacsgergely22 <113249160+kovacsgergely22@users.noreply.github.com>
2025-10-30 14:38:28 +0100

    add test case

--- src/main/java/uni/miskolc/Rover.java
+++ src/main/java/uni/miskolc/Rover.java
@@ -4,13 +4,16 @@ public class Rover {
     private int x;
     private int y;
     private Direction direction;
+    private final int planetWidth;
+    private final int planetHeight;
 
-    public Rover() {}
 
-    public Rover(int x, int y, Direction direction) {
+    public Rover(int x, int y, Direction direction, int planetWidth, int planetHeight) {
         this.x = x;
         this.y = y;
         this.direction = direction;
+        this.planetWidth = planetWidth;
+        this.planetHeight = planetHeight;
     }
 
     public int getX() {
@@ -22,6 +25,14 @@ public class Rover {
     }
     public Direction getDirection() {
         return this.direction;
+    }
+
+    public int getPlanetWidth() {
+        return planetWidth;
+    }
+
+    public int getPlanetHeight() {
+        return planetHeight;
     }
 
     public void setX(int x) {
--- src/test/java/uni/miskolc/RoverTest.java
+++ src/test/java/uni/miskolc/RoverTest.java
@@ -11,7 +11,7 @@ public class RoverTest {
     @Test
     void testRoverInitialState() {
 
-        Rover rover = new Rover(0, 0, Direction.NORTH);
+        Rover rover = new Rover(0, 0, Direction.NORTH,  PLANET_WIDTH, PLANET_HEIGHT);
 
         assertEquals(0, rover.getX());
         assertEquals(0, rover.getY());
@@ -21,35 +21,35 @@ public class RoverTest {
 
     @Test
     void testRoverCanTurnRightFromNorth() {
-        Rover rover = new Rover(0, 0, Direction.NORTH);
+        Rover rover = new Rover(0, 0, Direction.NORTH, PLANET_WIDTH, PLANET_HEIGHT);
         rover.execute("r"); // "r" = right
         assertEquals(Direction.EAST, rover.getDirection());
     }
 
     @Test
     void testRoverCanTurnRightFromEast() {
-        Rover rover = new Rover(0, 0, Direction.EAST);
+        Rover rover = new Rover(0, 0, Direction.EAST, PLANET_WIDTH, PLANET_HEIGHT);
         rover.execute("r");
         assertEquals(Direction.SOUTH, rover.getDirection());
     }
 
     @Test
     void testRoverCanTurnLeftFromNorth() {
-        Rover rover = new Rover(0, 0, Direction.NORTH);
+        Rover rover = new Rover(0, 0, Direction.NORTH,  PLANET_WIDTH, PLANET_HEIGHT);
         rover.execute("l"); // "l" = left
         assertEquals(Direction.WEST, rover.getDirection());
     }
 
     @Test
     void testRoverCanTurnLeftFromEast() {
-        Rover rover = new Rover(0, 0, Direction.WEST);
+        Rover rover = new Rover(0, 0, Direction.WEST,   PLANET_WIDTH, PLANET_HEIGHT);
         rover.execute("l");
         assertEquals(Direction.SOUTH, rover.getDirection());
     }
 
     @Test
     void testRoverHandlesMultipleTurnCommands() {
-        Rover rover = new Rover(0, 0, Direction.NORTH);
+        Rover rover = new Rover(0, 0, Direction.NORTH,  PLANET_WIDTH, PLANET_HEIGHT);
         rover.execute("rrl"); // N -> E -> S -> E
         assertEquals(Direction.EAST, rover.getDirection());
     }
@@ -68,6 +68,14 @@ public class RoverTest {
         rover.execute("f");
         assertEquals(1, rover.getX()); // X nő
         assertEquals(0, rover.getY());
+    }
+
+    @Test
+    void testRoverMovesBackwardSouth() {
+        Rover rover = new Rover(5, 5, Direction.SOUTH, PLANET_WIDTH, PLANET_HEIGHT);
+        rover.execute("b");
+        assertEquals(5, rover.getX());
+        assertEquals(6, rover.getY()); // Hátra 'S'-ből = Y nő
     }
 
 }

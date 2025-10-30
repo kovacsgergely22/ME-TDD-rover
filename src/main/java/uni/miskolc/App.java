df4124639dc2bf84474e46c8b00572d7d8718367
kovacsgergely22 <113249160+kovacsgergely22@users.noreply.github.com>
2025-10-30 11:05:26 +0100

    Initial commit

---
+++ src/main/java/uni/miskolc/App.java
@@ -0,0 +1,13 @@
+package uni.miskolc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
